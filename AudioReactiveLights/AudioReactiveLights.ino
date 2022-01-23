d4d67c5a1a7f1e0a9937a9484845202e05cb941e
IIAZYFII <18hussainasif@gmail.com>
2022-01-23 18:44:24 +0000

    intial set up for code and defined constant

--- AudioReactiveLights/AudioReactiveLights.ino
+++ AudioReactiveLights/AudioReactiveLights.ino
@@ -1,9 +1,20 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 100
+#define LED_PIN 2
+#define updateLEDS 3 //The amount of LEDS to update and move every loop
+
+CRGB leds[NUM_LEDS]; 
+
+int const micPin = A0;
 
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(50);
+  Serial.begin(9600);
 
 }
 
